f0310ff9f708b3920a9a12d9a347430d66d8b47d
Geisson Machado Antunes <geisonnm@hotmail.com>
2016-04-23 09:58:48 -0300

    Add Ajax Exemples

---
+++ Ajax/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src="js/app.js"></script>
+</head>
+<body>
+	<ul class="lista" >
+		<li class="a">1</li>
+		<li>2</li>
+		<li>3</li>
+	</ul>
+</body>
+</html>
---
+++ Ajax/js/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var array = new Array('Gremio', 'Sao Paulo', 'Corintians');
+
+// document.write(array.sort());
+// document.write(array.sort());
+
+var list = document.lista.a.value();
+console.log(list);
